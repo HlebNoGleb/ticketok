41648750416a77d2ea0b1e6162a3e1b4cfcdbee1
Gleb Hleb <gynyax5@gmail.com>
2023-07-05 00:22:26 +0300

    some fixes

--- lib/models/offline_event_database.dart
+++ lib/models/offline_event_database.dart
@@ -9,27 +9,27 @@ part 'offline_event_database.g.dart';
 @HiveType(typeId: 1)
 class Database{
   @HiveField(0)
-  final String category;
-
+  final String category; // удалить после нового апи и пересобрать сгенерированные скрипты
 
   @HiveField(1)
-  final List<Ticket> tickets; // удалить после раскоментирования и пересобрать сгенерированные скрипты
+  final List<Ticket> tickets; // удалить после нового апи и пересобрать сгенерированные скрипты
 
-  // @HiveField(2)
+  // @HiveField(1)
   // final int id;
 
   // @JsonKey(name: 'barcode_hash')
-  // @HiveField(3)
+  // @HiveField(2)
   // final String barcodeHash;
 
-  // @HiveField(4)
+  // @HiveField(3)
   // final String? time;
 
-  // @HiveField(5, defaultValue: false)
+  // @HiveField(4, defaultValue: false)
   // bool? isChecked = false;
 
-  // Database(this.category, this.tickets, this.id, this.barcodeHash, this.time);
-  Database(this.category, this.tickets);
+  // Database(this.id, this.barcodeHash, this.time);
+
+  Database(this.category, this.tickets); // удалить после нового апи и пересобрать сгенерированные скрипты
 
   factory Database.fromJson(Map<String, dynamic> json) => _$DatabaseFromJson(json);
 }
